82d1b128498b7a74e978927001ac84272ffdca5b
Breno <breno.branco@befreedev.com>
2022-12-18 23:07:09 -0300

    add custom fonts

--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import CardList from "../components/CardList";
 import SearchBox from "../components/SearchBox";
 import ErrorBoundry from "../components/ErrorBoundry";
+import './App.css'
 
 class App extends Component {
     constructor(){
